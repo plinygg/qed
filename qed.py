6eb07da9ba6f9767b014143fa58ce0ce180fae34
plinygg <119556552+plinygg@users.noreply.github.com>
2022-12-01 19:25:02 -0600

    additional organization changes

--- qed.py
+++ qed.py
@@ -1,5 +1,9 @@
 #Find the number of entries which are not divisible by 7 in the first one billion (10^9) rows of Pascal's triangle.
 
+# vars
+num_rows = 10**9
+base = 7
+
 # functions
 def int2(n, base):
     ret = []
@@ -16,20 +20,16 @@ def f(l, base):
     s = base * (base+1) // 2
     return r * (r+1) // 2 * s **(len(l)-1) + (r+1) * f(l[1:], base)
 
-     
-# vars
-num_rows = 10**9
-base = 7
-nondivisors = f(int2(num_rows, 7), 7)
-
-
 # Checks the percentage of the number of entries not divisible by num_rows and adds them to a list
 percentage = []
+nondivisors = f(int2(num_rows, 7), 7)
 def check_percentage(num_nondivisors):
     percentage.append(num_rows / nondivisors)
-    
+check_percentage(nondivisors)
 
-# prints
-# Prints the percentage of the number of entires not divisible by num_rows and the rounded version of the percentage
-print("This is the percentage of numbers within the first " + str(num_rows) + " rows that are not divisble by " + str(base) + ": " + str(percentage) + "%" + "Rounded: " + str(round(percentage[0], 3)) + "%")
+# running code
 print(f(int2(num_rows, 7), 7))
+
+# Prints the percentage of the number of entires not divisible by num_rows and the rounded version of the percentage
+print("This is the percentage of numbers within the first " + str(num_rows) + " rows that are not divisble by " + str(base) + ": " + str(percentage) + "% \nRounded: " + str(round(percentage[0], 3)) + "%")
+print("Number of entries which are not divisible by " + str(base) + "in the first " + str(num_rows) + " rows of Pascal's triangle: " + str(nondivisors))
